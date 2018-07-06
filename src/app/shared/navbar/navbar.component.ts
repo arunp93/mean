376952e9a23be89ba0a2460185177bdf8288322c
arunp93 <arunp93@gmail.com>
2018-07-06 11:52:51 +0530

    merging tadus proj

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 declare var $: any ;
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
@@ -16,7 +16,27 @@ export class NavbarComponent implements OnInit {
         $(this).toggleClass('is-active');
       });
     });
-
+    $(document).ready(function(){
+      const originalSize = $('body').css('font-size');
+     // reset
+      $('#sizereset').click(function(){
+     $('body').css('font-size', originalSize);
+      });
+      // Increase Font Size
+      $('#sizeup').click(function(){
+     let currentSize = $('body').css('font-size');
+     currentSize = parseFloat(currentSize) * 1.2;
+     $('body').css('font-size', currentSize);
+     return false;
+      });
+      // Decrease Font Size
+      $('#sizedown').click(function(){
+     const currentFontSize = $('body').css('font-size');
+     let currentSize = $('body').css('font-size');
+     currentSize = parseFloat(currentSize) * 0.8;
+     $('body').css('font-size', currentSize);
+     return false;
+      });
+   });
   }
-
 }
